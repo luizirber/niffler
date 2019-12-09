0be293f8ff3fad06571fdbb3aa9166461fff557d
Pierre Marijon <pmarijon@mpi-inf.mpg.de>
2019-12-09 22:31:26 +0100

    Cleaning of unnecessary functions and implementation of new API

--- src/lib.rs
+++ src/lib.rs
@@ -33,9 +33,8 @@ use failure::{Error, Fail};
 use flate2;
 
 /* standard use */
-use std::fs::File;
 use std::io;
-use std::io::{BufReader, BufWriter};
+use std::io::Read;
 
 enum_from_primitive! {
     #[repr(u64)]
@@ -54,47 +53,28 @@ pub enum OCFError {
     FeatureDisabled,
 }
 
-pub fn get_input(input_name: &str) -> Result<(Box<dyn io::Read>, CompressionFormat), Error> {
-    // choose std::io::stdin or open file
-    if input_name == "-" {
-        Ok((Box::new(get_readable(input_name)), CompressionFormat::No))
-    } else {
-        get_readable_file(input_name)
-    }
-}
-
-pub fn get_readable_file(
-    input_name: &str,
+pub fn get_reader(
+    in_stream: Box<dyn io::Read>,
 ) -> Result<(Box<dyn io::Read>, CompressionFormat), Error> {
-    let raw_input = get_readable(input_name);
-
     // check compression
-    let compression = get_compression(raw_input);
+    let (compression, in_stream) = read_compression(in_stream);
 
     // return readable and compression status
     match compression {
         CompressionFormat::Gzip => Ok((
-            Box::new(flate2::read::GzDecoder::new(get_readable(input_name))),
+            Box::new(flate2::read::GzDecoder::new(in_stream)),
             CompressionFormat::Gzip,
         )),
-        CompressionFormat::Bzip => new_bz2_decoder(get_readable(input_name)),
-        CompressionFormat::Lzma => new_lzma_decoder(get_readable(input_name)),
-        CompressionFormat::No => Ok((Box::new(get_readable(input_name)), CompressionFormat::No)),
+        CompressionFormat::Bzip => new_bz2_decoder(in_stream),
+        CompressionFormat::Lzma => new_lzma_decoder(in_stream),
+        CompressionFormat::No => Ok((in_stream, CompressionFormat::No)),
     }
 }
 
-pub fn get_readable(input_name: &str) -> Box<dyn io::Read> {
-    match input_name {
-        "-" => Box::new(BufReader::new(io::stdin())),
-        _ => Box::new(BufReader::new(
-            File::open(input_name)
-                .unwrap_or_else(|_| panic!("Can't open input file {}", input_name)),
-        )),
-    }
-}
-
-fn get_compression(mut in_stream: Box<dyn io::Read>) -> CompressionFormat {
-    let mut buf = vec![0u8; 5];
+fn read_compression<'a>(
+    mut in_stream: Box<dyn io::Read>,
+) -> (CompressionFormat, Box<dyn io::Read>) {
+    let mut buf = [0u8; 5];
 
     in_stream
         .read_exact(&mut buf)
@@ -104,8 +84,10 @@ fn get_compression(mut in_stream: Box<dyn io::Read>) -> CompressionFormat {
     for (i, item) in buf.iter().enumerate().take(5) {
         five_bit_val |= (u64::from(*item)) << (8 * (4 - i));
     }
+
     if CompressionFormat::from_u64(five_bit_val) == Some(CompressionFormat::Lzma) {
-        return CompressionFormat::Lzma;
+        let cursor = io::Cursor::new(buf);
+        return (CompressionFormat::Lzma, Box::new(cursor.chain(in_stream)));
     }
 
     let mut two_bit_val: u64 = 0;
@@ -113,9 +95,12 @@ fn get_compression(mut in_stream: Box<dyn io::Read>) -> CompressionFormat {
         two_bit_val |= (u64::from(*item)) << (8 * (1 - i));
     }
 
+    let cursor = io::Cursor::new(buf);
     match CompressionFormat::from_u64(two_bit_val) {
-        e @ Some(CompressionFormat::Gzip) | e @ Some(CompressionFormat::Bzip) => e.unwrap(),
-        _ => CompressionFormat::No,
+        e @ Some(CompressionFormat::Gzip) | e @ Some(CompressionFormat::Bzip) => {
+            (e.unwrap(), Box::new(cursor.chain(in_stream)))
+        }
+        _ => (CompressionFormat::No, Box::new(cursor.chain(in_stream))),
     }
 }
 
@@ -178,45 +163,18 @@ cfg_if! {
     }
 }
 
-pub fn get_output(
-    output_name: &str,
+pub fn get_writer(
+    out_stream: Box<dyn io::Write>,
     format: CompressionFormat,
 ) -> Result<Box<dyn io::Write>, Error> {
     match format {
         CompressionFormat::Gzip => Ok(Box::new(flate2::write::GzEncoder::new(
-            get_writable(output_name),
+            out_stream,
             flate2::Compression::best(),
         ))),
-        CompressionFormat::Bzip => new_bz2_encoder(get_writable(output_name)),
-        CompressionFormat::Lzma => new_lzma_encoder(get_writable(output_name)),
-        CompressionFormat::No => Ok(Box::new(get_writable(output_name))),
-    }
-}
-
-pub fn choose_compression(
-    input_compression: CompressionFormat,
-    compression_set: bool,
-    compression_value: &str,
-) -> CompressionFormat {
-    if !compression_set {
-        return input_compression;
-    }
-
-    match compression_value {
-        "gzip" => CompressionFormat::Gzip,
-        "bzip2" => CompressionFormat::Bzip,
-        "lzma" => CompressionFormat::Lzma,
-        _ => CompressionFormat::No,
-    }
-}
-
-fn get_writable(output_name: &str) -> Box<dyn io::Write> {
-    match output_name {
-        "-" => Box::new(BufWriter::new(io::stdout())),
-        _ => Box::new(BufWriter::new(
-            File::create(output_name)
-                .unwrap_or_else(|_| panic!("Can't open output file {}", output_name)),
-        )),
+        CompressionFormat::Bzip => new_bz2_encoder(out_stream),
+        CompressionFormat::Lzma => new_lzma_encoder(out_stream),
+        CompressionFormat::No => Ok(Box::new(out_stream)),
     }
 }
 
@@ -228,48 +186,108 @@ mod test {
     const GZIP_FILE: &'static [u8] = &[0o037, 0o213, 0o0, 0o0, 0o0];
     const BZIP_FILE: &'static [u8] = &[0o102, 0o132, 0o0, 0o0, 0o0];
     const LZMA_FILE: &'static [u8] = &[0o375, 0o067, 0o172, 0o130, 0o132];
+    const LOREM_IPSUM: &'static [u8] = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies scelerisque diam, a scelerisque enim sagittis at.";
 
-    #[test]
-    fn compression_from_file() {
-        assert_eq!(
-            get_compression(Box::new(GZIP_FILE)),
-            CompressionFormat::Gzip
-        );
-        assert_eq!(
-            get_compression(Box::new(BZIP_FILE)),
-            CompressionFormat::Bzip
-        );
-        assert_eq!(
-            get_compression(Box::new(LZMA_FILE)),
-            CompressionFormat::Lzma
-        );
+    mod compression_format_detection {
+        use super::*;
+
+        #[test]
+        fn gzip() {
+            let (compression, _) = read_compression(Box::new(GZIP_FILE));
+            assert_eq!(compression, CompressionFormat::Gzip);
+        }
+
+        #[test]
+        fn bzip() {
+            let (compression, _) = read_compression(Box::new(BZIP_FILE));
+            assert_eq!(compression, CompressionFormat::Bzip);
+        }
+
+        #[test]
+        fn lzma() {
+            let (compression, _) = read_compression(Box::new(LZMA_FILE));
+            assert_eq!(compression, CompressionFormat::Lzma);
+        }
     }
 
-    #[test]
-    fn compression_from_input_or_cli() {
-        assert_eq!(
-            choose_compression(CompressionFormat::Gzip, false, "_"),
-            CompressionFormat::Gzip
-        );
-        assert_eq!(
-            choose_compression(CompressionFormat::Bzip, false, "_"),
-            CompressionFormat::Bzip
-        );
-        assert_eq!(
-            choose_compression(CompressionFormat::Lzma, false, "_"),
-            CompressionFormat::Lzma
-        );
-        assert_eq!(
-            choose_compression(CompressionFormat::No, true, "gzip"),
-            CompressionFormat::Gzip
-        );
-        assert_eq!(
-            choose_compression(CompressionFormat::No, true, "bzip2"),
-            CompressionFormat::Bzip
-        );
-        assert_eq!(
-            choose_compression(CompressionFormat::No, true, "lzma"),
-            CompressionFormat::Lzma
-        );
+    mod compress_uncompress {
+        use super::*;
+
+        #[test]
+        fn gzip() {
+            {
+                let wfile =
+                    std::fs::File::create("tmp.gz").expect("We can't create tmp.file for test");
+
+                let mut writer = get_writer(Box::new(wfile), CompressionFormat::Gzip).unwrap();
+                writer
+                    .write_all(LOREM_IPSUM)
+                    .expect("Error durring write of data");
+            }
+
+            let rfile = std::fs::File::open("tmp.gz").expect("We can't read tmp.file for test");
+            let (mut reader, compression) =
+                get_reader(Box::new(rfile)).expect("Error in reading of tmp.file");
+
+            assert_eq!(compression, CompressionFormat::Gzip);
+
+            let mut buffer = Vec::new();
+            reader
+                .read_to_end(&mut buffer)
+                .expect("Error durring reading");
+            assert_eq!(LOREM_IPSUM, buffer.as_slice());
+        }
+
+        #[cfg(feature = "bz2")]
+        #[test]
+        fn bzip() {
+            {
+                let wfile =
+                    std::fs::File::create("tmp.bz2").expect("We can't create tmp.file for test");
+
+                let mut writer = get_writer(Box::new(wfile), CompressionFormat::Bzip).unwrap();
+                writer
+                    .write_all(LOREM_IPSUM)
+                    .expect("Error durring write of data");
+            }
+
+            let rfile = std::fs::File::open("tmp.bz2").expect("We can't read tmp.file for test");
+            let (mut reader, compression) =
+                get_reader(Box::new(rfile)).expect("Error in reading of tmp.file");
+
+            assert_eq!(compression, CompressionFormat::Bzip);
+
+            let mut buffer = Vec::new();
+            reader
+                .read_to_end(&mut buffer)
+                .expect("Error durring reading");
+            assert_eq!(LOREM_IPSUM, buffer.as_slice());
+        }
+
+        #[cfg(feature = "lzma")]
+        #[test]
+        fn lzma() {
+            {
+                let wfile =
+                    std::fs::File::create("tmp.xz").expect("We can't create tmp.file for test");
+
+                let mut writer = get_writer(Box::new(wfile), CompressionFormat::Lzma).unwrap();
+                writer
+                    .write_all(LOREM_IPSUM)
+                    .expect("Error durring write of data");
+            }
+
+            let rfile = std::fs::File::open("tmp.xz").expect("We can't read tmp.file for test");
+            let (mut reader, compression) =
+                get_reader(Box::new(rfile)).expect("Error in reading of tmp.file");
+
+            assert_eq!(compression, CompressionFormat::Lzma);
+
+            let mut buffer = Vec::new();
+            reader
+                .read_to_end(&mut buffer)
+                .expect("Error durring reading");
+            assert_eq!(LOREM_IPSUM, buffer.as_slice());
+        }
     }
 }
