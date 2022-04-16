d7ba3c53aefdbb9e08a0d5d42f2eedcb98262ff7
Pierre Marijon <pierre@marijon.fr>
2022-04-16 13:56:15 +0200

    Add buffer mode 2 a read buffer around niffler read with uncompressed data

--- src/bin/json.rs
+++ src/bin/json.rs
@@ -9,7 +9,13 @@ fn main() {
     let json_path = std::env::args().nth(2).unwrap();
     println!("{} {}", buffer, json_path);
     let mut count = 0;
-    if buffer == "1" {
+    if buffer == "2" {
+        let value: serde_json::Value = serde_json::from_reader(std::io::BufReader::new(
+            niffler::from_path(json_path).unwrap().0,
+        ))
+        .unwrap();
+        count += value.as_object().unwrap().len();
+    } else if buffer == "1" {
         let value: serde_json::Value =
             serde_json::from_reader(niffler::from_path(json_path).unwrap().0).unwrap();
         count += value.as_object().unwrap().len();
